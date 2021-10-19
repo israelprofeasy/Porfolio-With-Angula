1d46a0100d1aab36fb6b433a0d45c36ba4bfe61b
Israel Oluwafemi <israeloluwafemi44@gmail.com>
2021-10-20 00:49:08 +0100

    add s to api url

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
   constructor(private http: HttpClient) { }
-  getData(){
-    const url = "http://portfolio-dotnet.herokuapp.com/api/v1/biodata/3";
+  getData(): Observable<any>{
+    const url = "https://portfolio-dotnet.herokuapp.com/api/v1/biodata/3";
     return this.http.get(url).pipe(
       shareReplay()
     )
